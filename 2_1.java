8e3f27a839096f0fe6e202e28c6d8c214ed7a1f1
Huang <judezhu@repun.(none)>
2014-01-09 18:49:47 -0800

    modified 2_1.

--- 2_1.java
+++ 2_1.java
@@ -1,5 +1,51 @@
+//Todo: String should be replaced by generic class
+
+public class Node {
+	private Node next;
+	private char data;
+
+	public Node(){
+		next = null;
+		data = "";
+	}
+
+	public char getData(){
+		return data;
+	}	
+
+	private void setData(String str){
+		data = str;
+	}
+
+	public Node getNext(){
+		Node = next;
+	}
+
+}
+
+
+public class SingleLinkedList(){
+	private Node head;
+	public SingleLinkedList(){
+		head = null;
+	}
+	
+	public Node getFirst(){
+	
+	}
+
+	public SingleLinkedList deleteNode(char ch){
+
+	}
+
+}
 
 public class RemoveDuplicate {
+
+
+	public static void removeDuplicate(){
+		
+	}
 
 	//public static 
 	/**
@@ -7,7 +53,12 @@ public class RemoveDuplicate {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+                Scanner input = new Scanner(System.in);
+                System.out.println("Please enter your string:");
+                String in = input.nextLine();
+                char content[] = in.toCharArray();
+                System.out.println(replaceSpace(content));
+                input.close();
 	}
 
 }
